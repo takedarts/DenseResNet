1568c72362413fc36c65f020366f00ece153e9cc
Atsushi TAKEDA <atsushi@takedarts.jp>
2020-11-07 20:50:14 +0900

    support extended classes

--- src/utils/optimizer.py
+++ src/utils/optimizer.py
@@ -7,12 +7,20 @@ NORM_CLASSES = set([
     nn.BatchNorm3d,
 ])
 
-BIAS_CLASSES = set([
+CONV_CLASSES = set([
     nn.Conv1d,
     nn.Conv2d,
     nn.Conv3d,
     nn.Linear,
 ])
+
+
+def _isinstance(obj, classes):
+    for cls in classes:
+        if isinstance(obj, cls):
+            return True
+
+    return False
 
 
 def create_optimizer(model, lr, weight_decay, bias_decay=False):
@@ -22,20 +30,20 @@ def create_optimizer(model, lr, weight_decay, bias_decay=False):
             lr=lr, weight_decay=weight_decay, momentum=0.9, nesterov=True)
 
     norm_names = set()
-    bias_names = set()
+    conv_names = set()
     nodecay_params = []
     decay_params = []
 
     for name, module in model.named_modules():
-        if type(module) in NORM_CLASSES:
+        if _isinstance(module, NORM_CLASSES):
             norm_names.add(name)
-        elif type(module) in BIAS_CLASSES:
-            bias_names.add(f'{name}.bias')
+        elif _isinstance(module, CONV_CLASSES):
+            conv_names.add(f'{name}.bias')
 
     for name, param in model.named_parameters():
         if not param.requires_grad:
             continue
-        elif name.rsplit('.', maxsplit=1)[0] in norm_names or name in bias_names:
+        elif name.rsplit('.', maxsplit=1)[0] in norm_names or name in conv_names:
             nodecay_params.append(param)
         else:
             decay_params.append(param)
